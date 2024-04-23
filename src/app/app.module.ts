4e7179b10905cbbfbe3353bef8e345fc51f11d27
FarrelHI <99933100+farrel-hi@users.noreply.github.com>
2024-04-23 08:29:46 +0700

    delete unnnecessary component and change body bg colour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
-import { TitleTextComponent } from './title-text/title-text.component';
 import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
-import { DescTextComponent } from './desc-text/desc-text.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
@@ -23,9 +21,7 @@ const routes: Routes = [ //Create the Original ROute (login) and the destined pr
   declarations: [
     AppComponent,
     ButtonComponent,
-    TitleTextComponent,
     ProfilePictureComponent,
-    DescTextComponent,
     FormComponent,
     ProfileComponent,
     LoginComponent,
